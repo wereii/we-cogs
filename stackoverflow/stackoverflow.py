8450d6105931850ece201523ac0100434fe09274
wereii <tomas.svo722@gmail.com>
2019-11-07 22:04:13 +0100

    Reboot stackoverflow from Kixiron

---
+++ stackoverflow/stackoverflow.py
@@ -0,0 +1,43 @@
+import re
+
+import discord
+from redbot.core import commands
+from urllib.parse import quote
+
+# Copyright 2018 Kixiron
+# Edits 2019: Cog rebooted for v3 by Wereii
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+
+class StackOverflow(commands.Cog):
+    """ Ask questions from the mystical being which all knowledge flows from """
+
+    @commands.command()
+    async def stackoverflow(self, ctx: commands.Context, *, question: str = None):
+        """ Enlighten yourself """
+
+        if not question:
+            await ctx.send_help()
+            return
+
+        baseURL = "https://stackoverflow.com/search?q="
+        finallURL = baseURL + quote(question)
+
+        embed = discord.Embed(color=0x1e2dd4)
+        embed.set_author(name="StackOverflow says",
+                      url=finallURL)
+
+        await ctx.send(embed=embed)
